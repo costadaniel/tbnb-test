38a021d567f9c459f21262c94021c001bfd82f5f
costadaniel <costadaniel@alu.ufc.br>
2020-09-14 16:52:15 -0300

    Initial App update

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.15.6/api/product/",
+  baseURL: "http://192.168.15.10/api/product/",
   timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
